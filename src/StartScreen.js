f00c3d9f6401bbf83ec7a2b9f914d44654cc2bb5
Justin <jstn.anthony1@gmail.com>
2022-12-07 14:00:37 -0500

    added new stlying to start screen & end game screen

--- src/StartScreen.js
+++ src/StartScreen.js
@@ -17,25 +17,24 @@ const StartScreen = (props) =>{
  
 
         return(
-        <div className="main-end-screen-container">
+        <div className="main-start-screen-container">
             {/* <h1>{props.correctAnswer}</h1> */}
-            <div className="end-screen-background">
-                <div className="end-screen-container">
-                    <section className="end-screen-title"><h2>Welcome to "Who said that?"</h2>
+            <div className="start-screen-background">
+                <div className="start-screen-container">
+                    <section className="start-screen-title"><h2>Welcome to "Who said that?"</h2>
                     <p>Stranger Things Edition!</p>
                     <p>Guess which characer said each quote!</p>
                     </section>
-                    {/* <section className="end-screen-body"><p>{props.questionsAsked}</p></section> */}
-                    <section className="end-screen-footer">
+                    <section className="start-screen-footer">
                         <form onSubmit={usersChoice}>
                             <p>Pick how many questions you'd like to answer!</p>
-                            <div className="input-container">
+                            <div className="start-input-container">
                                 <div className="radio-container">
-                                    <input type="radio" id="option1" name="questions" onClick={handleStartClick} value={4}></input>
+                                    <input type="radio" id="option1" name="questions" onClick={handleStartClick} value={5}></input>
                                     <label htmlFor="option1">5 Questions</label>
                                 </div>
                                 <div className="radio-container">
-                                    <input type="radio" id="option2" name="questions" onClick={handleStartClick} value={9}></input>
+                                    <input type="radio" id="option2" name="questions" onClick={handleStartClick} value={10}></input>
                                     <label htmlFor="option2">10 Questions</label>
                                 </div>
                             </div>
